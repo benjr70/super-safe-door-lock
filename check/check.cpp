65e6845e7a040dbcab1a7964401dc5e3c0a19111
Kirby Swenson <gravydogpig@gmail.com>
2018-03-24 07:04:30 -0500

    No idea why check.h.gch keeps getting created. There's lots more work to do.

---
+++ check/check.cpp
@@ -0,0 +1,30 @@
+#include <"check.h">
+
+// not much yet, but a good place to start
+
+namespace CheckNS
+{
+	Check::Check
+	{
+		readUsers();
+		readLog();
+		recordCurrentAttempt();
+	}
+	
+	void Check::readUsers()
+	{
+		std::string userLine;
+	}
+	
+	void Check::readLog()
+	{
+		
+		std::string logLine;
+	}
+	
+	void Check::recordCurrentAttempt()
+	{
+		
+	}
+}
+
